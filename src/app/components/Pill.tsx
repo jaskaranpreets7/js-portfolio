4b1c721d9e56a0ab19fd259a90b06165f90c83bf
Jaskaranpreet Singh <jaskaransingh123@gmail.com>
2025-06-18 23:55:49 -0700

    add string as type

--- src/app/components/Pill.tsx
+++ src/app/components/Pill.tsx
@@ -1,5 +1,5 @@
 'use client'
-export default function Pill({ text }: { text: any }) {
+export default function Pill({ text }: { text: string }) {
   return (
     <span className="inline-flex items-center justify-center px-4 py-1 rounded-full text-sm font-medium text-white bg-white/10 dark:bg-gray-800 backdrop-blur-sm shadow-sm hover:shadow-md hover:bg-white/20 transition-all duration-200">
       {text}
